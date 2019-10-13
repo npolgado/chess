c135f4a60809217074c5fec4f0a84d503a6cd797
Nicholas Olgado <nicholas831@gmail.com>
2019-10-12 22:42:38 -0700

    red vs black setup
    merge with erics most recent. added colors plus changed text color for clarity.

--- game.py
+++ game.py
@@ -1,19 +1,7 @@
 import pygame
 
 NUM_BLOCKS = 30
-
 SQ_SZ = 25
-
-
-side = SQ_SZ * NUM_BLOCKS
-size = (side, side)
-screen = pygame.display.set_mode(size)
-
-
-pygame.init()
-pygame.display.set_caption('Chess')
-
-
 
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
@@ -21,6 +9,12 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 
+side = SQ_SZ * NUM_BLOCKS
+size = (side, side)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption('Chess')
+screen.fill(WHITE)
 
 board = [[5, 2, 3, 9, 6, 3, 2, 5],
          [1]*8, [0]*8, [0]*8, [0]*8, [0]*8, [1]*8,
@@ -28,9 +22,6 @@ board = [[5, 2, 3, 9, 6, 3, 2, 5],
 
 def draw_board():
     global board
-
-    screen.fill(WHITE)
-
     pygame.font.init()  # you have to call this at the start,
     # # if you want to use this module.
     # myfont = pygame.font.SysFont('Comic Sans MS', 15)
@@ -43,17 +34,32 @@ def draw_board():
     for i in range (a):
         for j in range(a):
             len = (side/a)
-            # pygame.draw.rect(screen, BLACK, (i * len + len / 4, j * len + len / 4, len-1, len-1))
-            # pygame.draw.rect(screen, RED, (i *len +len/4, j * len +len/4, len*(4/5), len*(4/5)))
+            if (i+j) % 2 == 0:
+                pygame.draw.rect(screen, RED, (i*len+len/4, j * len +len/4, len*(4/5), len*(4/5)))
+            else:
+                pygame.draw.rect(screen, BLACK, (i*len+len/4, j * len +len/4, len-1, len-1))
             myfont = pygame.font.SysFont('Comic Sans MS', 15)
-            textsurface = myfont.render(str(board[j][i]), False, BLACK)
+            textsurface = myfont.render(str(board[j][i]), False, GREEN)
             screen.blit(textsurface, (i *len +len/4, j *len +len/4))
 
     pygame.display.update()
 
-    pygame.time.wait(5000)
-
 draw_board()
+pygame.display.flip()
 
-while True:
-    pass
+#USED TO FIND RED VS BLACK###########
+import numpy as np
+a = np.zeros(shape=(8,8))
+for i in range(8):
+    for j in range(8):
+        a[i, j] = i+j
+print(a)
+#####################################
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
