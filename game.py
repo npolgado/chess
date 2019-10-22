1d14ea277c9d6e42308cdbfe58a966e2132b4231
Nicholas Olgado <nicholas831@gmail.com>
2019-10-22 16:54:00 -0400

    More Work
    Added init function, images folder, and placeholder for is_check function

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 NUM_BLOCKS = 30
 SQ_SZ = 25
@@ -8,27 +9,34 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+PIECES = {
+    0: 'Empty',    1: 'Pawn',
+    2: 'Bishop',    3: 'Knight',
+    5: 'Rook',    6: 'King',
+    9: 'Queen'
+}
 
-side = SQ_SZ * NUM_BLOCKS
-size = (side, side)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption('Chess')
-screen.fill(WHITE)
-
-board = [[5, 2, 3, 9, 6, 3, 2, 5],
+BOARD = [[5, 2, 3, 9, 6, 3, 2, 5],
          [1]*8, [0]*8, [0]*8, [0]*8, [0]*8, [1]*8,
          [5, 2, 3, 9, 6, 3, 2, 5]]
 
+side = SQ_SZ * NUM_BLOCKS
+size = (side, side)
+
+def is_check():
+    '''Checks to see if the gamestate is in check/checkmate, and flags'''
+    pass
+
+def init():
+    global screen
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption('Chess')
+    screen.fill(WHITE)
+
 def draw_board():
-    global board
+    global BOARD, screen
     pygame.font.init()  # you have to call this at the start,
-    # # if you want to use this module.
-    # myfont = pygame.font.SysFont('Comic Sans MS', 15)
-    # textsurface = myfont.render('SCORE: ' + str(score), False, BLACK)
-    # textsurface2 = myfont.render('HI:    ' + str(hiScore), False, BLACK)  # 'HI:    '+ str(hiScore)
-    # screen.blit(textsurface, (side - 100, 10))
-    # screen.blit(textsurface2, (side - 100, 30))
 
     a = 8
     for i in range (a):
@@ -40,27 +48,32 @@ def draw_board():
                 pygame.draw.rect(screen, BLACK, (i*len, j*len , len-1, len-1))
 
             myfont = pygame.font.SysFont('Comic Sans MS', 50)
-            textsurface = myfont.render(str(board[j][i]), False, GREEN)
+            textsurface = myfont.render(str(BOARD[j][i]), False, GREEN)
             screen.blit(textsurface, (i*len, j*len))
 
     pygame.display.update()
 
-draw_board()
-pygame.display.flip()
+    # # if you want to use this module.
+    # myfont = pygame.font.SysFont('Comic Sans MS', 15)
+    # textsurface = myfont.render('SCORE: ' + str(score), False, BLACK)
+    # textsurface2 = myfont.render('HI:    ' + str(hiScore), False, BLACK)  # 'HI:    '+ str(hiScore)
+    # screen.blit(textsurface, (side - 100, 10))
+    # screen.blit(textsurface2, (side - 100, 30))
 
-#USED TO FIND RED VS BLACK###########
-import numpy as np
-a = np.zeros(shape=(8,8))
-for i in range(8):
-    for j in range(8):
-        a[i, j] = i+j
-print(a)
-#####################################
+if __name__ == '__main__':
+    init()
+    draw_board()
+    pygame.display.flip()
+    running = True
 
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        '''GAME LOGIC'''
+        #Player 1's Turn
+        #Is Check? (Check for Checkmate or Check)
+        #Player 2's Turn
+        #Is Check? (Check for Checkmate or Check or not escaped previous Check)
+        #Repeat ^^
