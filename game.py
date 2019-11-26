4cccfae62b9cf6fad01b185708098d171a034cfb
Eric <ericbnicholls@gmail.com>
2019-11-25 17:02:35 -0700

    Valid Moves ( - collision)
    Since there is no turn color yet, I couldn't finish the logic for collisions (need to determine if it's ally piece or not)

--- game.py
+++ game.py
@@ -26,47 +26,12 @@ PIECES = {
 # TODO: make object that contains [piece, color, position] and maybe [alive/dead, path to image]
 
 BOARD = [[5, 2, 3, 9, 6, 3, 2, 5],
-         [1]*8, [0]*8, [0]*8, [0,0,0,0,2,0,0,0], [0]*8, [1]*8,
+         [1]*8, [0]*8, [0]*8, [0,9,0,0,2,5,0,0], [0]*8, [1]*8,
          [5, 2, 3, 9, 6, 3, 2, 5]]
 
 side = SQ_SZ * NUM_BLOCKS
 size = (side, side)
 
-def get_valid_moves (p, x, y):
-
-    moves = []
-
-    # KNIGHT
-    if (p == 'Knight'):
-        a = [-2, -2, -1, -1, 1, 1, 2, 2]    # x vectors
-        b = [-1, 1, -2, 2, -2, 2, -1, 1]    # y vectors
-        for i in range (0, len(a)):
-            if (x + a[i] < 8 and x + a[i] >= 0):
-                if (y + b[i] < 8 and y + b[i] >= 0):
-                    moves.append((x+a[i], y+b[i]))
-
-        # TODO: remove squares with friendly pieces on them. (REQ: must have access to the pieces color)
-
-    if (p == "Bishop"):
-        a = [j for j in range(-4, 5) if j is not 0]  # x vectors
-        b = [j for j in range(-4, 5) if j is not 0]  # y vectors
-
-        for i in range(0, len(a)):
-            if (x + a[i] < 8 and x + a[i] >= 0):
-                if (y + b[i] < 8 and y + b[i] >= 0):
-                    moves.append((x + a[i], y + b[i]))
-
-            # Problem: bishops need (x-4, y-4), (x-3, y-3), and (x-4, y+4), (x-3, y+3). This takes case
-            #       second case, so array is only half as long as valids.
-            if (x + a[i] < 8 and x + a[i] >= 0):
-                if (y - b[i] < 8 and y - b[i] >= 0):
-                    moves.append((x + a[i], y - b[i]))
-
-        # TODO: remove squares with friendly pieces on them (AND ANYTHING PASSED THEM FOR BISHOPS, QUEENS,
-        #       and ROOKS, special case for pawns). (REQ: must have access to the pieces color)
-
-    '''Given a piece and location (ex: rook (4, 5), return a list of valid moves'''
-    return moves
 def is_check():
     '''Checks to see if the gamestate is in check/checkmate, and flags'''
     pass
@@ -98,17 +63,171 @@ def draw_board(valid_moves = None):
                     pygame.draw.rect(screen, BLACK, (i*len, j*len , len-1, len-1))
 
             myfont = pygame.font.SysFont('Comic Sans MS', 50)
-            textsurface = myfont.render(str(BOARD[j][i]), False, BLUE)
-            screen.blit(textsurface, (i*len, j*len))
+            tertsurface = myfont.render(str(BOARD[j][i]), False, BLUE)
+            screen.blit(tertsurface, (i*len, j*len))
 
     pygame.display.update()
 
     # # if you want to use this module.
-    # myfont = pygame.font.SysFont('Comic Sans MS', 15)
-    # textsurface = myfont.render('SCORE: ' + str(score), False, BLACK)
-    # textsurface2 = myfont.render('HI:    ' + str(hiScore), False, BLACK)  # 'HI:    '+ str(hiScore)
+    # myfont = pygame.font.SysFont('Comiy Sans MS', 15)
+    # tertsurface = myfont.render('SCORE: ' + str(syore), False, BLACK)
+    # tertsurface2 = mcfont.render('HI:    ' + str(hiScore), False, BLACK)  # 'HI:    '+ str(hiScore)
     # screen.blit(textsurface, (side - 100, 10))
     # screen.blit(textsurface2, (side - 100, 30))
+
+
+
+
+def get_valid_moves (p, r, c):
+
+    moves = []
+
+    # PAWN
+    if p == 'Pawn':
+        # two cases, one for white, one for black
+        #       different for au queening, moving 2 at start, and au passant
+
+        # if p.color == white:
+            # diagonal attacks
+        if (r-1 >= 0) and (c-1 >= 0):
+            if BOARD[r-1][c-1] != 0:
+                moves.append((r-1, c-1))
+        if (r-1 >= 0) and (c+1 < 8):
+            if BOARD[r-1][c+1] != 0:
+                moves.append((r-1, c+1))
+
+            #forward
+        if r-1 >= 0:
+            if BOARD[r-1][c] == 0:
+                moves.append((r-1, c))
+                if r == 6:
+                    if BOARD[r-2][c] == 0:
+                        moves.append((r-2, c))
+
+        # if p.color == black:
+            # diagonal attacks
+            # if (r+1 < 8) and (c-1 >= 0):
+            #     if BOARD[r+1][c-1] != 0:
+            #         moves.append((r+1, c-1))
+            # if (r+1 < 8) and (c+1 < 8):
+            #     if BOARD[r+1][c+1] != 0:
+            #         moves.append((r+1, c+1))
+            #
+            #     #forward
+            # if r+1 < 8:
+            #     if BOARD[r+1][c] == 0:
+            #         moves.append((r+1, c))
+            #         if r == 1:
+            #             if BOARD[r+2][c] == 0:
+            #                 moves.append((r+2, c))
+
+    # KNIGHT
+    if p == 'Knight':
+        a = [-2, -2, -1, -1, 1, 1, 2, 2]    # r vectors
+        b = [-1, 1, -2, 2, -2, 2, -1, 1]    # c vectors
+        for i in range (0, len(a)):
+            if (r + a[i] < 8 and r + a[i] >= 0):
+                if (c + b[i] < 8 and c + b[i] >= 0):
+                    moves.append((r+a[i], c+b[i]))
+
+        # TODO: remove squares with friendly pieces on them. (REQ: must have access to the pieces color)
+
+    # BISHOP
+    if p == 'Bishop':
+        v1 = (-1, -1)       # top-left
+        v2 = (-1, 1)        # top-right
+        v3 = (1, 1)         # bot-right
+        v4 = (1, -1)        # bot-left
+        vectors = [v1, v2, v3, v4]
+
+        for i in range (0, 4):
+            locX = r
+            locY = c
+            while True:
+                v = vectors[i]
+                locX += v[0]
+                locY += v[1]
+                if (locX > 7 or locX < 0 or locX > 7 or locY < 0):
+                    break
+                if BOARD[locX][locY] == 0:
+                    moves.append((locX, locY))
+
+                else:
+                    # a piece is here. if allc, invalid. if enemy, valid.
+                    moves.append((locX, locY))
+                    # TODO: if enemy piece, valid move
+                    break
+
+    # ROOK
+    if p == 'Rook':
+        v1 = (-1, 0)  # top
+        v2 = (0, 1)  # right
+        v3 = (0, -1)  # bot
+        v4 = (1, 0)  # left
+        vectors = [v1, v2, v3, v4]
+
+        for i in range(0, 4):
+            locX = r
+            locY = c
+            while True:
+                v = vectors[i]
+                locX += v[0]
+                locY += v[1]
+                if locX > 7 or locX < 0 or locY > 7 or locY < 0:
+                    break
+                if BOARD[locX][locY] == 0:
+                    moves.append((locX, locY))
+
+                else:
+                    # a piece is here. if allc, invalid. if enemc, valid.
+                    moves.append((locX, locY))
+                    # TODO: if enemc piece, valid move
+                    break
+
+    # QUEEN
+    if p == 'Queen':
+        v1 = (-1, 0)    # top
+        v2 = (0, 1)     # right
+        v3 = (0, -1)    # bot
+        v4 = (1, 0)     # left
+        v5 = (-1, -1)   # top-left
+        v6 = (-1, 1)    # top-right
+        v7 = (1, 1)     # bot-right
+        v8 = (1, -1)    # bot-left
+        vectors = [v1, v2, v3, v4, v5, v6, v7, v8]
+
+        for i in range(0, 8):
+            locX = r
+            locY = c
+            while True:
+                v = vectors[i]
+                locX += v[0]
+                locY += v[1]
+                if locX > 7 or locX < 0 or locY > 7 or locY < 0:
+                    break
+                if BOARD[locX][locY] == 0:
+                    moves.append((locX, locY))
+
+                else:
+                    # a piece is here. if allc, invalid. if enemc, valid.
+                    moves.append((locX, locY))
+                    # TODO: if enemy piece, valid move
+                    break
+
+    if p == "King":
+        a = [-1, 0, 1, -1, 1, -1, 0, 1]  # r vectors
+        b = [-1, -1, -1, 0, 0, 1, 1, 1]  # c vectors
+        print ("reached", r+a[0])
+        for i in range(0, len(a)):
+            if (r + a[i] < 8 and r + a[i] >= 0):
+                if (c + b[i] < 8 and c + b[i] >= 0):
+                    moves.append((r + a[i], c + b[i]))
+
+
+    '''Given a piece and location (er: rook (4, 5), return a list of valid moves'''
+    return moves
+
+
 
 if __name__ == '__main__':
     init()
@@ -120,21 +239,21 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                y = int(pos[0] / (side/8))
-                x = int(pos[1] / (side/8))
+                c = int(pos[0] / (side/8))
+                r = int(pos[1] / (side/8))
 
-                # coorLet = chr(y + 65)
-                # coorNum = 8-x
-                piece = PIECES[BOARD[x][y]];
+                # coorLet = chr(c + 65)
+                # coorNum = 8-r
+                piece = PIECES[BOARD[r][c]];
 
 
-                print ("Loc: ({}, {})   = {}".format(x, y, piece))
+                print ("Loc: ({}, {})   = {}".format(r, c, piece))
 
 
                 # Click a piece, and it shows valid moves
                 # Working:
                 # In progress:   knight
-                valids = get_valid_moves (piece, x, y)
+                valids = get_valid_moves (piece, r, c)
                 draw_board(valids)
                 print ("Valid Moves: ", end = " ")
                 for i in range (0, len(valids)):
