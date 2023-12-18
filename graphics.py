ea84c0a0d1382066b8d696dadf017457c0c48be4
nicholas <nicholas831@gmail.com>
2023-12-18 01:11:20 -0800

    draws inital board and timer

--- graphics.py
+++ graphics.py
@@ -12,20 +12,36 @@ from pygame.locals import (
     QUIT,
 )
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+BOARD_LIGHT_COLOR = (255, 206, 158)
+BOARD_DARK_COLOR = (209, 139, 71)
 
 class Graphics:
-    def __init__(self, board = None, game = None) -> None:
+    def __init__(self, board = None, game = None, display_index=0) -> None:
         # setup pygame for a chess board of 8x8 squares, and a display of 800x800 pixels
         pygame.init()
         self.board_size = 8
         self.square_size = 100
-        self.display_size = self.board_size * self.square_size
         self.border_size = 100
-        self.display = pygame.display.set_mode((self.display_size, self.display_size))
+        self.display_size = self.board_size * self.square_size + (2*self.border_size)
+
+        self.framerate = 30
+
+        self.display = pygame.display.set_mode(
+            size=(self.display_size, self.display_size),
+            flags=pygame.RESIZABLE|pygame.SCALED,
+            display=display_index
+        )
+        self.clock = pygame.time.Clock()
+
         self.board = board
         self.game = game
         self.piece_images = {}
+
         self.running = True
+
         self.load_images()
         self.init_board()
         pygame.display.update()
@@ -36,12 +52,51 @@ class Graphics:
 
     def init_board(self):
         # TODO: initialize board
+
         # Fill the background BLACK
         self.display.fill((0, 0, 0))
+        
+        # draw board grid (TODO: add row column labels)
+        self.draw_grid()
+        
+        # initialize game timer drawing
+        self.game_timer = pygame.font.SysFont('Arial', 30)
+        self.display.blit(self.game_timer.render('00:00', True, WHITE), (self.border_size, self.border_size/2))
+
 
     def draw(self, board, gs):
+        # handle pygame events like quitting or aftergame controls
         self.handle_game_events()
+
+        # update screen
         pygame.display.flip()
+
+        # Ensure program maintains a rate of 30 frames per second
+        self.clock.tick(self.framerate)
+
+    def draw_grid(self):
+        # board size is display size - 2*border size
+        grid_len = self.display_size - self.border_size
+        
+        counter = 0 
+
+        # start x at border on lfet side, and increment by total board size
+        for x in range(self.border_size, grid_len, self.square_size):
+            for y in range(self.border_size, grid_len, self.square_size):
+
+                # if x is even, y is odd, or vice versa, then the square is light
+                if counter % 2 == 0:
+                    color = BOARD_LIGHT_COLOR
+                else:
+                    color = BOARD_DARK_COLOR
+                
+                rect = pygame.Rect(x, y, self.square_size, self.square_size)
+                self.display.fill(color, rect)
+                pygame.draw.rect(self.display, color, rect, 1)
+
+                counter += 1
+
+            counter += 1
 
     def handle_game_events(self):
         # Look at every event in the queue
