0e9aa3ff6d38862f5e343ba02d2725be20c81864
nicholas <nicholas831@gmail.com>
2023-12-21 20:32:36 -0800

    nothing

--- nick_AI.py
+++ nick_AI.py
@@ -1,16 +1,16 @@
 import numpy as np
+import threading
+import asyncio
+import time
+import random
 
 class AI :
-    def __init__(self, game, col):
-        self.game = game    # game object to get information about the game
-        self.color = col    # color the ai is playing (0 white, 1 black)
-
+    def __init__(self):
         self.target_move = None
-
+        
     # has to be non-blocking, return none and thread the processing of the move
     def recieve(self, move: str) -> None:
         self.target_move = None
-        pass
 
-    def get_target_move(self) -> tuple:
+    def get_target_move(self, valid_moves):
         return self.target_move
