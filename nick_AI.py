0362fdeb7b0db1bc1030050ce436f7effb380a5a
nicholas <nicholas831@gmail.com>
2023-12-22 00:48:45 -0800

    nick ai threading

--- nick_AI.py
+++ nick_AI.py
@@ -1,28 +1,62 @@
 import numpy as np
 from __init__ import *
 from game_state import GameState
+import threading
+import random
+import time
 
 class AI :
     def __init__(self):
         self.g = GameState(init_empty_board())
+
+        self.thinking = False
+        
         self.target_move = None
-        self.opponent_move = None
+        
+        self.last_move = None
+
+        self.threading_process = None
+        self.lock = threading.Lock()
         
     # has to be non-blocking, return none and thread the processing of the move
-    def recieve(self, move: str) -> None:
-        self.target_move = None
+    def recieve(self, move):
+        with self.lock:
+            if move == None or self.thinking:
+                self.target_move = None
+                return
+            
+            # start threaded process
+            threading.Thread(
+                target=self.process_move,
+                args=(move,)
+            ).start()
+            
+            self.thinking = True
+            self.target_move = None
 
-    def get_target_move(self):
+    def get_target_move(self, valid_moves):
         return self.target_move
     
-    def process_move(self, move: str) -> None:
-        # 1. update board given move
-        self.board = make_move(self.board, move)
-        
-        # 2. update opponent_move
-        self.opponent_move = move
+    def process_move(self, move: str):
+        with self.lock:
+            # Update game board state from opponent move
+            self.g.update(move)
+            self.last_move = move
+            self.g.get_valid_moves()
+            
+            # using self.g.valid_moves, find a random move where the keys are the from_pos, and the values are the to_pos
+            from_pos = random.choice(list(self.g.valid_moves.keys()))
+            while len(self.g.valid_moves[from_pos]) == 0:
+                from_pos = random.choice(list(self.g.valid_moves.keys()))
+            
+            # Construct Target Move
+            to_pos = random.choice(self.g.valid_moves[from_pos])           
+            target_move = translate_move_t2s(from_pos[0], from_pos[1], to_pos[0], to_pos[1])
+            # print(f"AI move: {from_pos} -> {to_pos}")
+            # print(f"AI move: {target_move}")
 
-        # 3. get valid moves
-        self.g.get_valid_moves(self.board, self.opponent_move)
-        
-        pass
+            # Update game board state from target move
+            self.g.update(target_move)
+
+            self.target_move = target_move
+            self.thinking = False
