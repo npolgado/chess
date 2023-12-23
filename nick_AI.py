9136e0ca364157e4e6eca64187238eead76d138b
nicholas <nicholas831@gmail.com>
2023-12-23 00:05:51 -0800

    more ai work

--- nick_AI.py
+++ nick_AI.py
@@ -12,7 +12,6 @@ class AI :
         self.thinking = False
         
         self.target_move = None
-        
         self.last_move = None
 
         self.threading_process = None
@@ -20,11 +19,7 @@ class AI :
         
     # has to be non-blocking, return none and thread the processing of the move
     def recieve(self, move):
-        with self.lock:
-            if move == None or self.thinking:
-                self.target_move = None
-                return
-            
+        with self.lock:            
             # start threaded process
             threading.Thread(
                 target=self.process_move,
@@ -32,31 +27,90 @@ class AI :
             ).start()
             
             self.thinking = True
-            self.target_move = None
 
     def get_target_move(self, valid_moves):
-        return self.target_move
+        tmp = self.target_move
+        self.target_move = None
+        return tmp
     
-    def process_move(self, move: str):
+    def process_move(self, move):
         with self.lock:
-            # Update game board state from opponent move
-            self.g.update(move)
-            self.last_move = move
-            self.g.get_valid_moves()
+
+            # CASE: WAITING FOR OTHER PLAYER1
+            if move == None and self.g.turn != 1: 
+                self.thinking = False
+                return
             
-            # using self.g.valid_moves, find a random move where the keys are the from_pos, and the values are the to_pos
-            from_pos = random.choice(list(self.g.valid_moves.keys()))
-            while len(self.g.valid_moves[from_pos]) == 0:
-                from_pos = random.choice(list(self.g.valid_moves.keys()))
-            
-            # Construct Target Move
-            to_pos = random.choice(self.g.valid_moves[from_pos])           
+            # CASE: PLAYER HAS MOVED
+            if move != None:
+                self.g.update(move)
+                self.last_move = move
+
+            # update valid moves to only include current players moves
+            valid_moves = self.g.get_valid_moves()
+            player_valid_moves = {}
+
+            print("VALID MOVES")
+            print(valid_moves)
+            print("\n\n")
+
+            for i in valid_moves:
+                piece = self.g.board[i[0]][i[1]]
+                        
+                # ignore pieces without valid moves
+                if valid_moves[i] == []:
+                    continue
+                
+                # if black ignore lowercase keys
+                if self.g.bool_turn and piece.islower():
+                    continue
+                
+                # if white, ignore upper case keys, 
+                elif not self.g.bool_turn and piece.isupper():
+                    continue
+                
+                player_valid_moves[i] = valid_moves[i]
+
+            print("VALID MOVES")
+            print(player_valid_moves)
+            print("\n\n")
+
+            # Find a random move where the keys are the from_pos, and the values are the to_pos
+            from_pos = random.choice(list(player_valid_moves.keys()))
+            to_pos = random.choice(player_valid_moves[from_pos]) 
+
+            # Construct Target Move          
             target_move = translate_move_t2s(from_pos[0], from_pos[1], to_pos[0], to_pos[1])
-            # print(f"AI move: {from_pos} -> {to_pos}")
-            # print(f"AI move: {target_move}")
+            print(f"AI move: {from_pos} -> {to_pos}")
+            print(f"AI move: {target_move}")
 
             # Update game board state from target move
             self.g.update(target_move)
 
+            # Set target move to be returned
             self.target_move = target_move
+
+            # Signal process is done
             self.thinking = False
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
+
+    def softmax(self, x):
+        exp_x = np.exp(x - np.max(x))  # Subtracting np.max(x) for numerical stability
+        return exp_x / exp_x.sum(axis=0)
+
+    def forward(self):
+        pass
+
+    def attention(self, q, k, v):
+        # "Scaled Dot-Product Attention"
+        # multi-head attention given a query, key, and value
+        # We compute the dot products of the query with all keys,
+        # divide each by √dk, and apply a softmax function 
+        # to obtain the weights on the values.
+
+        pass
