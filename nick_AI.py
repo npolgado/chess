df73e5c1524ee55e52adf2d603ab1a3d118a0478
Nicholas Olgado <nicholas831@gmail.com>
2023-12-24 22:24:32 -0800

    new ai just dropped

--- nick_AI.py
+++ nick_AI.py
@@ -9,15 +9,59 @@ class AI :
     def __init__(self):
         self.g = GameState()
         
-        self.target_move = None
-        self.last_move = None
-        
     # Recieve updated move from the other player ai
     def recieve(self, move):
-        self.g.update(move)
+        # self.g.update(move)
+        pass  
 
     def get_random_move(self, valid_moves):
         keys = list(valid_moves.keys())
+
+        # get all possible moves evaluations
+        evals = {}
+
+        # print_board(self.g.board)
+        curr_board = self.g.board
+
+        for i in keys:
+            for j in valid_moves[i]:
+                # print(f"\n\nfrom {i} to {j}")
+                pos_from = i
+                pos_to = j
+                
+                notation = translate_move_t2s(*pos_from, *pos_to)
+                # print(f"{notation}")
+
+                b = make_move(curr_board, notation)
+                b_eval = evaluate_board(b)
+                evals[notation] = b_eval
+                # print(b_eval)
+                # print(f"{notation} @ {b_eval}")
+
+        # find the best move
+        # if white, get highest evaluation
+        if self.g.player_turn:
+            # print(f"best as white")
+            max_value = np.max(list(evals.values()))
+            best = [key for key, value in evals.items() if value == max_value]
+
+        # if black, get the lowest eval
+        else:
+            # print(f"best as black")
+            min_value = np.min(list(evals.values()))
+            best = [key for key, value in evals.items() if value == min_value]
+
+        # extract to string
+        if len(best) > 1:
+            best = random.choice(best)
+        else:
+            best = best[0]
+
+        print(f"{self.g.player_turn}\t{self.g.turn_num}:\t{best} @ {evals[best]}")
+        self.g.update(best)
+        return best
+
+        # pick randomly
         from_pos = random.choice(keys)
         
         count = 0
